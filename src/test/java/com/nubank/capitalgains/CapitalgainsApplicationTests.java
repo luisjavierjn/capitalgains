7e3de6e3f5f4dcae66d069e0a0126219a477f315
Luis Jimenez <luis.jimenez@nordstrom.com>
2022-05-01 10:11:51 -0500

    first commit

---
+++ src/test/java/com/nubank/capitalgains/CapitalgainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nubank.capitalgains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitalgainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
